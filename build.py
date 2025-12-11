d277b9d5fbbb725a9e47291731b231d17596233d
Skye Prince <spiralixp@gmail.com>
2025-12-10 16:34:32 -0800

    sort and format updates

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from datetime import datetime
 
 SRC_FOLDER = 'src'
 UPDATES_FOLDER = 'updates'
@@ -10,11 +11,35 @@ STYLESHEET = 'style.css'
 
 UPDATES_PLACEHOLDER = "<!--#UPDATES-->"
 
-def update_to_div(filename: str, content: str) -> str:
+def update_to_div(name: str, content: str) -> str:
 	'''
 	converts an update file (in `UPDATES_FOLDER`) to a homepage entry
 	'''
-	return f'<div class="update-item"><h2>{filename}</h2><p>{content}</p></div>'
+	return f'<div class="update-item"><h2>{name}</h2><p>{content}</p></div>'
+
+def make_updates() -> str | None:
+	'''
+	sorts and compiles the contents of the `UPDATES_FOLDER` into HTML
+	'''
+	updates_path = Path(UPDATES_FOLDER)
+	if not updates_path.exists():
+		print('updates folder not found')
+		return
+	update_files: list[tuple[Path, datetime]] = []
+	for path in updates_path.iterdir():
+		try:
+			update_files.append((path, datetime.strptime(path.stem, "%d-%m-%Y")))
+		except ValueError:
+			print(f'update {path} does not match the format dd-mm-yyyy');
+			return
+	update_files.sort(key=lambda pair: pair[1], reverse=True)
+	updates = []
+	for (update_path, update_date) in update_files:
+		with open(update_path, 'r') as update:
+			title = update_date.strftime('%B %-d, %Y')
+			update_div = update_to_div(title, update.read())
+			updates.append(update_div)
+	return str.join('\n', updates)
 
 def make_homepage() -> str | None:
 	'''
@@ -29,18 +54,8 @@ def make_homepage() -> str | None:
 		if (UPDATES_PLACEHOLDER not in content):
 			print(f'placeholder {UPDATES_PLACEHOLDER} not found in index.html')
 			return
-		# compile updates folder
-		updates_path = Path(UPDATES_FOLDER)
-		if not updates_path.exists():
-			print('updates folder not found')
-			return
-		updates = []
-		for update_path in updates_path.iterdir():
-			with open(update_path, 'r') as update:
-				update_div = update_to_div(update_path.name, update.read())
-				updates.append(update_div)
-		# sub in the compiled updates
-		return content.replace(UPDATES_PLACEHOLDER, str.join('\n', updates))
+		if (updates := make_updates()) is not None:
+			return content.replace(UPDATES_PLACEHOLDER, updates)
 	
 def build():
 	build_path = Path(BUILD_FOLDER)
