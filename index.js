72da9360af576f863f912316a0a2938da4e4246f
Skye Prince <spiralixp@gmail.com>
2025-12-13 12:02:55 -0800

    more reorganization

--- index.js
+++ index.js
@@ -1,65 +1,77 @@
-import http from "http";
-import fs from "fs";
-import path from "path";
+import http from "node:http";
+import fs from "node:fs";
+import path from "node:path";
 
 import moment from "moment"
 
-const UPDATES_PLACEHOLDER = "<!--#UPDATES-->";
+// TODO: minify
 
-const homepage = fs.readFileSync("src/index.html");
-const blog = fs.readFileSync("src/blog.html");
-const stylesheet = fs.readFileSync("src/style.css");
+function getUpdateHtml(date, text) {
+	const heading = date.format("MMMM Do, YYYY");
+	return `<div class="update"><h2>${heading}</h2><div>${text}</div></div>`;
+}
 
-const favicon = fs.readFileSync("images/favicon.svg");
-const logo = fs.readFileSync("images/logo.svg");
-const title = fs.readFileSync("images/title.svg");
+function getUpdates() {
+	return fs.readdirSync("updates")
+		.map(filename => {
+			const date = moment(path.basename(filename, ".html"));
+			const content = fs.readFileSync(`updates/${filename}`);
+			return [date, content];
+		})
+		.sort((a, b) => a[0].isBefore(b[0]) ? 1 : -1)
+		.map(pair => getUpdateHtml(pair[0], pair[1]));
+}
 
-const updates = fs.readdirSync("updates")
-	.map(filename => {
-		const date = moment(path.basename(filename, ".html"));
-		const content = fs.readFileSync(`updates/${filename}`);
-		return [date, content];
-	})
-	.sort((a, b) => a[0].isBefore(b[0]) ? 1 : -1)
-	.map(pair => `<div class="update"><h2>${pair[0].format("MMMM Do, YYYY")}</h2><div>${pair[1]}</div></div>`)
-	.join("");
+function getHomepage() {
+	const rawPage = fs.readFileSync("templates/homepage.html");
+	return rawPage.toString().replace("$updates", getUpdates().join("\n"));
+}
 
-const index = homepage.toString().replace(UPDATES_PLACEHOLDER, updates);
+const styles = Object.fromEntries(
+	fs.readdirSync("styles")
+		.map(filename => [filename, fs.readFileSync(`styles/${filename}`)])
+);
+
+const images = Object.fromEntries(
+	fs.readdirSync("images")
+		.map(filename => [filename, fs.readFileSync(`images/${filename}`)])
+);
+
+const homepage = getHomepage();
 
 const server = http.createServer((req, res) => {
 	if (req.method !== null) {
-		console.log(`${req.method} ${req.url}`);
+		console.log(`[${moment().format("HH:mm:ss")}] ${req.method} ${req.url}`);
 	}
 });
 
-const routes = {
-	"/": index,
-}
-
 server.addListener("request", (req, res) => {
-	if (req.url === "/") {
-		res.writeHead(200, { "content-type": "text/html" });
-		res.end(index);
+	if (req.url === null || req.method !== "GET") {
+		return;
 	}
-	else if (req.url === "/style.css") {
+	const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`);
+	const args = url.pathname.split("/").slice(1).filter(arg => arg.length > 0);
+	const path = args.join("/");
+	console.log(args);
+	if (path.endsWith(".css") && Object.hasOwn(styles, args.at(-1))) {
 		res.writeHead(200, { "content-type": "text/css" });
-		res.end(stylesheet);
+		res.end(styles[args.at(-1)]);
+		return;
 	}
-	else if (req.url === "/blog") {
+	if (path.endsWith(".svg") && Object.hasOwn(images, path)) {
+		res.writeHead(200, { "content-type": "image/svg+xml" });
+		res.end(images[path]);
+		return;
+	}
+	if (path === "/") {
 		res.writeHead(200, { "content-type": "text/html" });
-		res.end(blog);
+		res.end(homepage);
+		return;
 	}
-	else if (req.url === "/favicon.ico") {
-		res.writeHead(200, { "content-type": "image/svg+xml" });
-		res.end(favicon);
-	}
-	else if (req.url === "/logo.svg") {
-		res.writeHead(200, { "content-type": "image/svg+xml" });
-		res.end(logo);
-	}
-	else if (req.url === "/title.svg") {
-		res.writeHead(200, { "content-type": "image/svg+xml" });
-		res.end(title);
+	if (path === "blog") {
+		res.writeHead(200, { "content-type": "text/html" });
+		res.end(fs.readFileSync("templates/blogpost.html"));
+		return;
 	}
 });
 
